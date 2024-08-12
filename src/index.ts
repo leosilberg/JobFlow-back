79407cc93f405261ceac153119a35bfc6274e307
SivanPesahov <sivanp27540@gmail.com>
2024-08-12 09:13:10 +0300

    f

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import jobRoutes from "./routes/job.routes.ts";
 import linkedinRoutes from "./routes/linkedin.routes.ts";
 import aiRoutes from "./routes/openai.routes.ts";
 import userRoutes from "./routes/user.routes.ts";
-
+import path from "path";
 
 const PORT = process.env.PORT || 3000;
 
@@ -20,11 +20,16 @@ async function main() {
 
   app.use(express.json());
   app.use(cors());
+  app.use(express.static("public"));
   app.use("/api/auth", authRoutes);
   app.use("/api/user", verifyToken, userRoutes);
   app.use("/api/job", verifyToken, jobRoutes);
   app.use("/api/linkedin", linkedinRoutes);
   app.use("/api/openai", verifyToken, aiRoutes);
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
   app.listen(PORT, () => {
     console.log(`index: Server listening on`, PORT);
   });
