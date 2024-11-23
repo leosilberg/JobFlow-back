2ba5d334e2f03dfed562d707ab821585d687c57b
leosilberg <ariel.silberg@gmail.com>
2024-11-23 14:37:53 +0200

    chore: add import sort plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default tseslint.config({
   ],
   rules: {
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars":"warn",
-    "@typescript-eslint/no-empty-object-type":"warn"
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-empty-object-type": "warn",
   },
 });
